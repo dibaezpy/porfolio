f61917fc8843ab8bc25c4b7f5007a805b1bf13ab
dibaezpy <155925756+dibaezpy@users.noreply.github.com>
2025-08-26 21:05:50 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
-import pnl  # tu módulo actual
+import pnl
 
 st.set_page_config(page_title="Panel de Módulos", layout="wide")
 
 st.sidebar.title("Navegación")
+st.sidebar.caption("build: app.py v1")   # ← marca rápida
 submodulo = st.sidebar.selectbox("📊 Submódulo Administración:", ["P&L"], index=0)
 
 if submodulo == "P&L":
-    pnl.show()   # llama al módulo P&L
+    pnl.show()
